ebca5737e20e19d7fa07c263c11084ae24afad7f
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2020-10-31 16:56:40 -0400

    React Bootstrap import and Footer, Header comp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React, { Fragment } from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <Fragment className='App'>
-      <h1>Welcome To TechShop</h1>
+    <Fragment>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome To TechShop</h1>
+        </Container>
+      </main>
+      <Footer />
     </Fragment>
   );
 };
