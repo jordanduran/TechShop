86664317ad150c8e5679c4cc2dae5e86a620ed9e
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2020-10-31 16:36:34 -0400

    React setup

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const App = () => {
+  return (
+    <Fragment className='App'>
+      <h1>Welcome To TechShop</h1>
+    </Fragment>
+  );
+};
+
+export default App;
